359f3fa14634394a7095b60b7f3b7c744263224a
zx <nick@Nicks-MacBook-Pro.local>
2025-09-22 15:19:09 +1200

    Add Node.js server for Azure deployment

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static('.'));
+
+// Route for root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Handle all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
